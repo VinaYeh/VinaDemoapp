7695af36a4d2310ea0c70ffbce3c79f3edd4fde0
Yutone Yeh <vina@Yutonede-MacBook-Pro.local>
2017-07-04 19:44:47 +0800

    Initial Commit

---
+++ Vina Frist App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vina Frist App
+//
+//  Created by Yutone Yeh on 2017/7/4.
+//  Copyright © 2017年 Yutone Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
